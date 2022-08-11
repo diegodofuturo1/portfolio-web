edfbb754b797411b0b173353298f8ff0d6998ec2
Diego Heleno <diegodofuturo1@gmail.com>
2022-08-11 19:58:21 -0300

    api page first version

--- service/index.ts
+++ service/index.ts
@@ -1,8 +1,8 @@
-import { signin, signout, signup, whoami } from "./auth";
-import { getEndpoints, getParamsByEndpoint, execute } from "./request";
+import { signin, signout, signup, whoami } from './auth';
+import { getEndpoints, getParamsByEndpoint, execute } from './request';
 
-export const url = "http://localhost:5000/";
-// export const url = "https://damp-brook-68706.herokuapp.com/";
+// export const url = "http://localhost:5000/";
+export const url = 'https://damp-brook-68706.herokuapp.com/';
 
 export default {
   auth: {
